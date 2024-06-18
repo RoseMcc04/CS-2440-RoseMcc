aedf22e414ec7a7d55668ffb5cd046a485e29ee0
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-18 11:26:35 -0400

    adding onto App.java

--- Week4/Inheritance/AccountsExample/App.java
+++ Week4/Inheritance/AccountsExample/App.java
@@ -1,5 +1,9 @@
 package Week4.Inheritance.AccountsExample;
 
-public class App {
-    
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        CheckingAccount checking = new CheckingAccount();
+    }    
 }
