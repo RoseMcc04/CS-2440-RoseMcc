690f4ac50c2197610b82de55780bc159b6ef78e0
RoseMcc04 <mccormacka@appstate.edu>
2024-07-02 11:39:38 -0400

    started linked list notes

---
+++ Week6/LinkedList/LinkedList.java
@@ -0,0 +1,24 @@
+package Week6.LinkedList;
+
+public class LinkedList 
+{
+    /*
+    Linked Lists vs. Arrays
+
+    - Arrays are stored in consecutive cells in RAM. You have to specify a
+      type and a count of the number of elements.
+    - Java understands the amount of space the type takes, so it scales the
+      int size by the number of elements in the array, thus creating the size
+      of memory for that array.
+    - A program can access a direct element of the array, along with Java 
+      knowing the initial position of the array in RAM.
+    - RAM is an advantage for arrays, but a drawback is that the size of an 
+      array is fixed. Another drawback is that it is expensive to insert in 
+      or delete an element. All elements from that point forward have to be 
+      shifted.
+    - Linked lists is a structure composed of nodes. In a singly-linked list, 
+      each node contains the link (RAM address) of the next node, along with a
+      piece of textual, object, or numeric data. The tail node's link is null
+      which signifies that it is the end of the list.
+    */    
+}
