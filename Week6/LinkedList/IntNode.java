98fb9c02f13e3fbb335874d7422045fe38f3429a
RoseMcc04 <mccormacka@appstate.edu>
2024-07-02 11:57:49 -0400

    need to remember to push files separately

---
+++ Week6/LinkedList/IntNode.java
@@ -0,0 +1,9 @@
+package Week6.LinkedList;
+
+public class IntNode 
+{
+    // Element stored in the current node
+    private int data;
+    // Refers to the next node in the list
+    private IntNode link;
+}
