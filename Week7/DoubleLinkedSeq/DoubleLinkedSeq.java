a42bc783b7e89ed694f1ec0eb5f06b380fd65d94
RoseMcc04 <mccormacka@appstate.edu>
2024-07-09 18:55:27 -0400

    will finish DoubleLinkedSeq notes later

--- Week7/DoubleLinkedSeq/DoubleLinkedSeq.java
+++ Week7/DoubleLinkedSeq/DoubleLinkedSeq.java
@@ -9,6 +9,12 @@ public class DoubleLinkedSeq
     // Fields
     
     /*
-     * 
+     * manyNodes --> keeps track of the number of nodes
+     * head --> reference to the head node
+     * tail --> reference to the tail node
+     * cursor --> reference to node with current element, or null if no 
+     *            current element
+     * precursor --> reference to node before current element, or null if
+     *               no current element
      */
 }
