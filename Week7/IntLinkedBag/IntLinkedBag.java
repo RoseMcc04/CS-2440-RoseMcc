9945472fdd8f0b129fdc9cc13936e4a14ea30cf9
RoseMcc04 <mccormacka@appstate.edu>
2024-07-09 15:33:16 -0400

    started IntLinkedBag notes

--- Week7/IntLinkedBag/IntLinkedBag.java
+++ Week7/IntLinkedBag/IntLinkedBag.java
@@ -6,5 +6,22 @@ package Week7.IntLinkedBag;
  */
 public class IntLinkedBag 
 {
-    
+    // Introduction
+
+    /*
+     * Back in Week 5, we covered the IntArrayBag class. We are using the
+     * same specification along with a few newer methods. We will learn how to
+     * implement these, along with looking at the algorithmic complexities
+     * to observe worst-case scenarios of collections-related methods. 
+     */
+
+     // Fields
+
+     /*
+      * Two instance variables --> head and manyNodes
+      * Invariants --> 
+      *     - Elements in the bag are stored in a linked list
+      *     - Head reference of list is stored in the instance variable head
+      *     - Total number of elements is stored in manyNodes
+      */
 }
