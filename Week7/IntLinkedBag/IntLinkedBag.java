39c4ea146b3c5e7f0a523be87443749455824a67
RoseMcc04 <mccormacka@appstate.edu>
2024-07-09 18:14:41 -0400

    decided to learn IntLinkedBag first lol

--- Week7/IntLinkedBag/IntLinkedBag.java
+++ Week7/IntLinkedBag/IntLinkedBag.java
@@ -24,4 +24,49 @@ public class IntLinkedBag
       *     - Head reference of list is stored in the instance variable head
       *     - Total number of elements is stored in manyNodes
       */
+
+      // Clarification
+
+      /*
+       * What values should head and manyNodes have when the bag is empty?
+       *    - null, 0
+       * 
+       * Why do we have a field for manynodes when where is a method in the
+       * IntNode class to compute the length of a list? There is a size method 
+       * in IntLinkedBag. Which should I use?
+       *    - It is inefficnet to walk the list and count nodes. The size
+       *      method should just return manyNodes.
+       * 
+       * Does the IntLinkedBag class need to perform a deep copy in the clone
+       * method, or is super.clone enough?
+       *    - It needs to perform a deep copy (list will need to be copied)
+       * 
+       * IntLinkedBag uses methods from IntNode for many of its tasks.
+       *    - listCopy method --> IntNode.listCopy(head)
+       */
+
+       // Results - Remove Method
+
+       /*
+        * Recall the parameter, remove, is the item to remove and the item has
+        * to be found before it can be removed.
+        *    - listSearch: IntNode n = IntNode.listSearch(head, target);
+        *
+        * With IntArrayBag, we removed an element by replacing it with the 
+        * last element in the array and decrement manyNodes by 1. 
+        *
+        * It replaces the data with the data in the head node and then removes
+        * the head node.
+        *
+        * targetNode = IntNode.listSearch(head, target);
+        * if (targetNode == full) return false;
+        * else
+        * {
+        *    targetNode.setData(head.getData());
+        *    head = head.getLink();
+        *    manyNodes--;
+        *    return true;
+        * }
+        *
+        */
 }
