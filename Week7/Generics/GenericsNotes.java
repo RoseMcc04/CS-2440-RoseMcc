3305703ca2bf4f0eb607e2658bb05d1a1f6e3a7d
RoseMcc04 <mccormacka@appstate.edu>
2024-07-10 15:18:35 -0400

    starting GenericsNotes.java

--- Week7/Generics/GenericsNotes.java
+++ Week7/Generics/GenericsNotes.java
@@ -6,5 +6,24 @@ package Week7.Generics;
  */
 public class GenericsNotes 
 {
-    
+    // Context
+
+    /*
+     * There are eight primitive types in Java.
+     * 
+     * 1. byte
+     * 2. short
+     * 3. int
+     * 4. long
+     * 5. float
+     * 6. double
+     * 7. char
+     * 8. boolean
+     * 
+     * However, everything else is just an object. All objects are part of a 
+     * heirarchy of Java objects and are all subclasses of the Object class. We
+     * have previously discussed methods of the Object class that are 
+     * inherited by all other classes but often have to be overidden to be
+     * useful. (Ex. equals(), toString(), compareTo(), hashCode())
+     */
 }
