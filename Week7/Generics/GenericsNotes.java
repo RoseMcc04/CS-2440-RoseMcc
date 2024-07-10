5ccb3e267e4a05ea9b236e7eb30b9235340fd985
RoseMcc04 <mccormacka@appstate.edu>
2024-07-10 15:32:16 -0400

    reorganized my notes

--- Week7/Generics/GenericsNotes.java
+++ Week7/Generics/GenericsNotes.java
@@ -26,4 +26,24 @@ public class GenericsNotes
      * inherited by all other classes but often have to be overidden to be
      * useful. (Ex. equals(), toString(), compareTo(), hashCode())
      */
+
+    // Container Classes
+
+    /*
+     * In the Java library, we have useful container classes like ArrayList, 
+     * LinkedList, and Vector which are all generic classes. When declaring 
+     * a generic object, you have to explain to Java which wrapper type you
+     * desire the structure to use. An example is...
+     * 
+     * ArrayList<String> a = new ArrayList<String>();
+     * 
+     * The type is supplied in angle brackets. Any type other than primitives
+     * will work. In terms of primitives, you will use their respective
+     * wrapper classes instead. Each primitive has a corresponding wrapper 
+     * class.
+     * 
+     * byte --> Byte; short --> Short; int --> Integer; long --> Long, 
+     * float --> Float; double --> Double; char --> Character; 
+     * boolean --> boolean
+     */
 }
