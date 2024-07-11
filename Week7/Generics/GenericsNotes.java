125a16c4105bd2fe6c7f77763af199eb94a7d52c
RoseMcc04 <mccormacka@appstate.edu>
2024-07-10 22:38:44 -0400

    more deep into GenericsNotes.java

--- Week7/Generics/GenericsNotes.java
+++ Week7/Generics/GenericsNotes.java
@@ -49,6 +49,55 @@ public class GenericsNotes
      * Java allows you to treat a wrapper variable as if it were a primitive 
      * with normal declarations. Integer x = new Integer(5); is declaring an
      * Integer object whereas Integer y = 7; will be used as a primitive by the
-     * compiler. 
+     * compiler. This is called an example of "autoboxing".
+     * 
+     * Integer z = x + y; We would now call this the process of "auto-unboxing".
+     * This is because we would be using an Integer object in an arithmetic 
+     * expression.
+     */
+
+    // Advantages/Disadvantages
+
+    /*
+     * Disadvantage --> some overhead involved in usage, and that extra work 
+     * causes Java to be less efficient.
+     * 
+     * Advantage --> you can turn primitives into object when genetric 
+     * containers are necessary. 
+     */
+
+    // Reason for Generics
+
+    /*
+     * Maybe we want to learn how to write a method for reversing the elements
+     * in a 1D array structure. We could choose from many primitives: chars, 
+     * doubles, bytes, float, booleans, etc. We could practice method 
+     * overloading, or we could make the method generic. 
+     * 
+     * When we want the type to be generic, programmers will typically either
+     * use 'T' or 'E' to denote the generic type. We then put this letter
+     * inside of a set of angle brackets. 
+     * 
+     * The Java Virtual Machine (JVM) has guidelines on how to properly use
+     * generic types. 
+     * 
+     * - You cannot use the "new" keyword to create an object of the generic
+     *   type. 
+     * 
+     * - You cannot create an array of the generic type. 
+     * 
+     * These restructions occur due to erasure, a compilation technique. This
+     * works in which the exact data type of a generic type in unknown at run
+     * time when a generic method is running. 
+     */
+
+    // Why Generic Instead of Object?
+
+    /*
+     * Declaration --> public static <T> T process(T val) 
+     *                 > public static Object process(Object val)
+     * 
+     * No casting! --> String s = process("vv"); 
+     *                 vs. String s = (String) process("vv");
      */
 }
