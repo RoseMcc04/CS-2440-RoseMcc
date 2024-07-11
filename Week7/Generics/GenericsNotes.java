be762fdcf59618f0274312de5d4bd3db820628f6
RoseMcc04 <mccormacka@appstate.edu>
2024-07-11 12:39:16 -0400

    using nested class to demonstrate understanding of generic methods and classes

--- Week7/Generics/GenericsNotes.java
+++ Week7/Generics/GenericsNotes.java
@@ -113,5 +113,39 @@ public class GenericsNotes
      * or T) in angle brackets immediately after the class name. 
      * 
      * Example --> public class Node<T> implements Cloneable
+     * 
+     * Method example --> 
      */
+
+    class Node<T> 
+    {
+        private T data;
+        private Node<T> link;
+
+        public Node(T data, Node<T> link) 
+        {
+            this.data = data;
+            this.link = link;
+        }
+
+        public void setData(T data) 
+        {
+            this.data = data;
+        }
+
+        public void setLink(Node<T> link) 
+        {
+            this.link = link;
+        }
+
+        public T getData() 
+        {
+            return this.data;
+        }
+
+        public Node<T> getLink() 
+        {
+            return this.link;
+        }
+    }
 }
