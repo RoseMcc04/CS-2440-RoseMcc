e19cf3451073ef7ecddfb68fb6ef54a5b5b9d72a
RoseMcc04 <mccormacka@appstate.edu>
2024-07-16 16:22:16 -0400

    finished IteratorNotes.java

--- Week8/WildcardsAndIterators/IteratorNotes.java
+++ Week8/WildcardsAndIterators/IteratorNotes.java
@@ -1,5 +1,21 @@
 package Week8.WildcardsAndIterators;
 
-public class IteratorNotes {
+public class IteratorNotes 
+{
+    // Iterable
     
+    /*
+     * Iterable is another generic interface in the Java library. It sounds 
+     * similar to Iterator, but does a different job. For a class to implement
+     * iterable, it must implement... public Iterator<T> iterator()
+     * 
+     * The method stated above returns an Iterator. This interface allows a 
+     * programmer to use a for/each loop to iterate over each collection.
+     * 
+     * Container classes implement the Iterable<E> interface and return an
+     * iterator that is built for the class. That means that typically, you
+     * will build a class that implements Iterator<E> to iterate over your 
+     * container class, then return a new object of that iterator in the 
+     * Iterable method. 
+     */
 }
