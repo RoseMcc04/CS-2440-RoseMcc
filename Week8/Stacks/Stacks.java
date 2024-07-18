5230cacfe05d976677c39c90170d1836cca326e0
RoseMcc04 <mccormacka@appstate.edu>
2024-07-17 21:42:18 -0400

    doing notes on the Stack data structure

---
+++ Week8/Stacks/Stacks.java
@@ -0,0 +1,42 @@
+package Week8.Stacks;
+
+/**
+ * @author Rose McCormack
+ * @version 18 July 2024
+ */
+public class Stacks 
+{
+    // Introduction 
+
+    /*
+     * A stack is a data structure of ordered items such that items can be 
+     * inserted and only removed at the end (called thed top). Stacks follow
+     * the LIFO (Last In, First Out) principle.
+     * 
+     *             Pop
+     *              ^
+     *             -
+     *            -
+     * Push --> [top node] --> [node] --> [node] --> [node]
+     *          ^
+     *         -
+     *        -
+     *   Stack Pointer
+     */
+
+    // Generic Specification of a Stack
+
+    /*
+     * public boolean isEmpty()
+     * 
+     * public E peek()
+     * 
+     * public E pop()
+     * 
+     * public void push(E item)
+     * 
+     * public int size()
+     */
+
+    // More Notes on Stacks
+}
