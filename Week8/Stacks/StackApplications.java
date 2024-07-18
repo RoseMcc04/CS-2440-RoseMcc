20e64c9a8faa6af6dba0654e0285ffb3d6942528
RoseMcc04 <mccormacka@appstate.edu>
2024-07-18 13:47:02 -0400

    did 6.2 example for class, will write logic in comments above for the next step

--- Week8/Stacks/StackApplications.java
+++ Week8/Stacks/StackApplications.java
@@ -1,10 +1,73 @@
 package Week8.Stacks;
 
+// Import generic collection for Stack data structure
+import java.util.Stack;
+
 /**
  * @author Rose McCormack
  * @version 18 July 2024
  */
 public class StackApplications 
 {
-    
+    /**
+     * @param expression String input the user wants to see if it is balanced 
+     * or not
+     * @return This method returns if the String input is balanced or not.
+     */
+    public static boolean isBalanced(String expression) 
+    {
+        /*
+         * Postcondition: A true return value indicates that the parentheses in
+         * the given expression are balanced. Otherwise, the return value is 
+         * false.
+         * 
+         * Characters other than  (), {}, and [] are ignored. 
+         */
+
+        // Char Names for Stack object
+        final char LEFT_NORMAL = '(';
+        final char RIGHT_NORMAL = ')';
+        final char LEFT_CURLY = '{';
+        final char RIGHT_CURLY = '}';
+        final char LEFT_SQUARE = '[';
+        final char RIGHT_SQUARE = ']';
+
+        // from java.util.Stack
+        Stack<Character> store = new Stack<Character>();
+        // An index into the String
+        int i;
+        // Change to true for mismatch
+        boolean failed = false;
+
+        for (i = 0; !failed && (i < expression.length()); i++) 
+        {
+            switch (expression.charAt(i)) 
+            {
+                case LEFT_NORMAL:
+                case LEFT_CURLY:
+                case LEFT_SQUARE:
+                    store.push(expression.charAt(i));
+                    break;
+                case RIGHT_NORMAL:
+                    if (store.isEmpty() || (store.pop() != LEFT_NORMAL)) 
+                    {
+                        failed = true;
+                    }
+                    break;
+                case RIGHT_CURLY:
+                    if (store.isEmpty() || (store.pop() != LEFT_CURLY)) 
+                    {
+                        failed = true;
+                    }
+                    break;
+                case RIGHT_SQUARE:
+                    if (store.isEmpty() || (store.pop() != LEFT_SQUARE)) 
+                    {
+                        failed = true;
+                    }
+                    break;
+            }
+        }
+        return (store.isEmpty() && !failed);
+    }
 }
