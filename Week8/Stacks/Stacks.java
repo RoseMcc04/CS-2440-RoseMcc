9c384dc4ec570e825f36dc31dba1469845550001
RoseMcc04 <mccormacka@appstate.edu>
2024-07-18 13:26:00 -0400

    added specification for generic methods in Stack.java

--- Week8/Stacks/Stacks.java
+++ Week8/Stacks/Stacks.java
@@ -49,4 +49,35 @@ public class Stacks
      * Stacks are useful, but the extent of their scope will be covered in 
      * future coursework.  
      */
+
+    // Further Into Specification
+
+    /*
+     * public Stack()
+     *      - Initializes an empty stack
+     * 
+     * public boolean isEmpty()
+     *      - Determines if the stack is empty
+     *      - If empty return true, otherwise false
+     * 
+     * public E peek()
+     *      - Gets the top item of the stack without removing it
+     *      - Returns the top item in the stack
+     *      - Throws EmptyStackException if empty
+     * 
+     * public E pop()
+     *      - Gets the top item and removes it from the stack
+     *      - Returns the value of the top item and deletes it
+     *      - Throws EmptyStackException if empty
+     * 
+     * public void push(E item)
+     *      - Pushes an item into the stack
+     *      - Throws OutOfMemoryError if there is insufficient memory
+     * 
+     * public int size()
+     *      - Accessor method to determine the number of items in this stack
+     *      - Returns the number of items in the stack
+     * 
+     * We can perform the following import as well --> java.util.EmptyStackException
+     */
 }
