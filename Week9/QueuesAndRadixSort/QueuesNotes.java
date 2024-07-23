8aca142e69cd3338a00acdaa64c964cacf8652da
RoseMcc04 <mccormacka@appstate.edu>
2024-07-23 18:57:54 -0400

    finished QueuesNotes.java

--- Week9/QueuesAndRadixSort/QueuesNotes.java
+++ Week9/QueuesAndRadixSort/QueuesNotes.java
@@ -1,6 +1,38 @@
 package Week9.QueuesAndRadixSort;
 
+/**
+ * @author Rose Adriana McCormack
+ * @version 23 July 2024
+ */
 public class QueuesNotes 
 {
-    
+    // Generic Queue
+
+    /*
+     * - A queue is a data structure of ordered items such that items can only
+     *   be inserted only at one end (called the rear) and removed at the other
+     *   end (called the front). Follows the FIFO principle (First-In-Last-Out).
+     * - We are in queues today; turn to get food in a cafeteria, line to pay
+     *   for groceries, and so forth. 
+     * 
+     * - Unline with Stack<E>, there is no Queue<E> class in the Java library, 
+     *   however, there is a Queue<E> interface implemented by several Java 
+     *   classes, LinkedList being among them. An example is listed below...
+     *   
+     *   Queue<Integer> q = new LinkedList<Integer>();
+     */
+
+    // Queue Methods
+
+    /*
+     * public boolean isEmpty()
+     * 
+     * public E peek() --> sometimes called examine()
+     * 
+     * public E remove() --> sometimes called dequeue() or poll()
+     * 
+     * public void add(E item) --> sometimes called enqueue() or insert()
+     * 
+     * public int size()
+     */
 }
