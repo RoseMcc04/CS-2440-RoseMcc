a1e06803d9d9c54fda9603bee8ba5167359a2106
RoseMcc04 <mccormacka@appstate.edu>
2024-07-23 11:16:26 -0400

    further into stack applications

--- Week9/StackApplications/StackApplications.java
+++ Week9/StackApplications/StackApplications.java
@@ -2,5 +2,20 @@ package Week9.StackApplications;
 
 public class StackApplications 
 {
-    
+    // Infix Notation
+
+    /*
+     * - Operators written in between operands
+     * - Ex. A * (B + C) / D ==> "First add B and C together, multiply by A
+     *   and then divide all of that by D"
+     * - We have rules (PEMDAS) to help us perform these calculations
+     */
+
+    // Postfix Notation
+
+    /*
+     * - Operators are written after their operands
+     * - Ex. A B C + * D /
+     * 
+     */
 }
