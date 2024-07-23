33742eb6d807cf0a4a4b6bb62fdd286509fc9e88
RoseMcc04 <mccormacka@appstate.edu>
2024-07-23 11:48:20 -0400

    doing examples for stack applications

--- Week9/StackApplications/StackApplications.java
+++ Week9/StackApplications/StackApplications.java
@@ -16,6 +16,34 @@ public class StackApplications
     /*
      * - Operators are written after their operands
      * - Ex. A B C + * D /
+     * - Order of operations is also left-to-right, and prackets cannot be
+     *   used to change this order. Because '+' is before the '*', it is the
+     *   first operation performed. Operands act on values immediately to the
+     *   left of them. We can make this more explicit: ( (A (B C +) *) D /)
+     */
+
+    // Prefix Notation
+
+    /*
+     * - Operators are written before their operands. 
+     * - Ex. / * A + B C D
+     * - Order of operations are right-to-left and the operators act on 
+     *   the two nearest values to the right. We can make this more explicit: 
+     *   (/ (* A(+ B C)) D)
+     * - Although prefix operators are "left-to-right", they use values to 
+     *   their right, and if these values themselves involve computations, then
+     *   this changes the order that the operators have to be evaluated in. 
+     */
+
+    // Practice 1:
+
+    /*
+     * 1. ((4 + 5) * (3 - (8 / 4)))
+     *  - Prefix: * + 4 5 - 3 / 8 4
+     *  - Postfix: 4 5 + * 3 - 8 4 /
      * 
+     * 2. ((8 - (5 * 2)) + (4 * 6))
+     *   - Prefix: 
+     *   - Postfix: 5 - 2 * 4 6 * + 
      */
 }
